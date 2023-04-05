66059f728ef3e9eab4891630ef5061cfbdc9c1f6
hyunseong moon <gustjd617@github.com>
2023-04-05 16:01:48 +0900

    commit test

---
+++ practice/src/main/java/MoonProject/practice/NoticeBoardPracticeApplication.java
@@ -0,0 +1,13 @@
+package MoonProject.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeBoardPracticeApplication.class, args);
+	}
+
+}
