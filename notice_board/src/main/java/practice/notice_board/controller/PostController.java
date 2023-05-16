c86e27db6b6bdf280005303ebf1348ce64dddf08
MoonHyunSeong <76519757+MoonHyunSeong@users.noreply.github.com>
2023-05-16 18:06:39 +0900

    게시글 작성 작업중

--- notice_board/src/main/java/practice/notice_board/controller/PostController.java
+++ notice_board/src/main/java/practice/notice_board/controller/PostController.java
@@ -1,12 +1,14 @@
 package practice.notice_board.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import practice.notice_board.domain.Post;
+import practice.notice_board.dto.PostDto;
 import practice.notice_board.service.PostService;
 
 import java.util.List;
@@ -21,17 +23,46 @@ public class PostController {
     @GetMapping("/board/{categoryName}")
     public String noticeBoard(@PathVariable String categoryName, Model model) {
 
+        System.out.println("categoryName = " + categoryName);
         List<Post> postByCategory = postService.getPostByCategory(categoryName);
 
         for (Post post : postByCategory) {
             System.out.println("post = " + post.toString());
         }
 
-        model.addAllAttributes(postByCategory);
+        model.addAttribute("categoryName", categoryName);
 
 
         return "/board/post";
     }
+
+    @GetMapping("/board/createPost")
+    public String createPostView(@RequestParam String categoryName, Model model) {
+
+        model.addAttribute("categoryName", categoryName);
+
+        return "board/createPost";
+    }
+
+    @PostMapping("/board/createPost")
+    public String createPost(@ModelAttribute PostDto newPost, HttpServletRequest request
+            , @RequestParam("categoryName") String categoryName) {
+
+        System.out.println("categoryName = " + categoryName);
+
+        HttpSession session = request.getSession(false);
+        String loginMemberId = (String) session.getAttribute("loginUserId");
+
+        log.info("post : {} MemberId : {} categoryName : {}",
+                newPost, loginMemberId, categoryName);
+        Boolean result = postService.createPost(newPost, loginMemberId, categoryName);
+        if (result == true) {
+            return "redirect:/board/post";
+        } else {
+            return "/welcomePage";
+        }
+    }
+
     /**
      * 1. categoryName을 가지고 list를 뽑아와서 모델에 담은 뒤 리턴.
      * 2. view로 categoryName을 던져서 뷰에서 js로 비동기 처리.
